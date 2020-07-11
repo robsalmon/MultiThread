dc86638e913eb447e6d20e9bd17e8b363bc09a10
Robert Salmon <robdavidsalmon@msn.com>
2020-07-11 19:48:57 +0100

    Example of two threads running in a windows form

--- MultiThread/Form1.Designer.cs
+++ MultiThread/Form1.Designer.cs
@@ -28,13 +28,48 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Thread1Button = new System.Windows.Forms.Button();
+            this.Thread2Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Thread1Button
+            // 
+            this.Thread1Button.Location = new System.Drawing.Point(12, 12);
+            this.Thread1Button.Name = "Thread1Button";
+            this.Thread1Button.Size = new System.Drawing.Size(75, 23);
+            this.Thread1Button.TabIndex = 0;
+            this.Thread1Button.Text = "Thread 1";
+            this.Thread1Button.UseVisualStyleBackColor = true;
+            this.Thread1Button.Click += new System.EventHandler(this.Thread1Button_Click);
+            // 
+            // Thread2Button
+            // 
+            this.Thread2Button.Location = new System.Drawing.Point(93, 12);
+            this.Thread2Button.Name = "Thread2Button";
+            this.Thread2Button.Size = new System.Drawing.Size(75, 23);
+            this.Thread2Button.TabIndex = 1;
+            this.Thread2Button.Text = "Thread 2";
+            this.Thread2Button.UseVisualStyleBackColor = true;
+            this.Thread2Button.Click += new System.EventHandler(this.Thread2Button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Thread2Button);
+            this.Controls.Add(this.Thread1Button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Thread1Button;
+        private System.Windows.Forms.Button Thread2Button;
     }
 }
 
--- MultiThread/Form1.cs
+++ MultiThread/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,60 @@ namespace MultiThread
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Random rd;
+
+        private void Thread1Button_Click(object sender, EventArgs e)
+        {
+            
+            //Start new thread
+
+            Thread thread = new Thread(t =>
+
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(50, this.Height);
+                    CreateGraphics().DrawEllipse(new Pen(Brushes.Red, 1), new Rectangle(width, height, 10, 10));
+                    //Delay
+                    Thread.Sleep(100);
+                }
+            }
+
+            )
+            {
+                IsBackground = true
+            };
+
+            thread.Start();
+          
+
+        }
+
+        private void Thread2Button_Click(object sender, EventArgs e)
+        {
+            //Start new thread
+            Thread thread = new Thread(t =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    int width = rd.Next(0, this.Width);
+                    int height = rd.Next(50, this.Height);
+                    this.CreateGraphics().DrawEllipse(new Pen(Brushes.Blue, 1), new Rectangle(width, height, 10, 10));
+
+
+                }
+            })
+            { IsBackground = true };
+            thread.Start();
+        }
+
+     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rd = new Random();
         }
     }
 }
